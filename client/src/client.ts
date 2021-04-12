1de3172118191d57a5881aac6c4e068a7132b791
Mike Franklin <293276+mikeemoo@users.noreply.github.com>
2021-04-12 17:23:51 +0000

    Removed unneccessary setTick

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,34 +1,10 @@
-let autoDriveEnabled = false;
-let autoDriveActive = false;
-const myPed = PlayerPedId();
 const waypointBlipId = GetWaypointBlipEnumId();
 
 RegisterCommand(
   "+autodrive",
   () => {
-    autoDriveEnabled = true;
-  },
-  false
-);
-
-RegisterCommand(
-  "-autodrive",
-  () => {
-    autoDriveEnabled = false;
-  },
-  false
-);
-
-RegisterKeyMapping("+autodrive", "Auto Drive", "keyboard", "b");
-
-setTick(() => {
-  const vehicleId = GetVehiclePedIsIn(myPed, false);
-  if (!vehicleId) {
-    autoDriveActive = false;
-    return;
-  }
-
-  if (autoDriveEnabled && !autoDriveActive) {
+    const myPed = PlayerPedId();
+    const vehicleId = GetVehiclePedIsIn(myPed, false);
     const blip = GetFirstBlipInfoId(waypointBlipId);
     if (blip) {
       const [x, y, z] = GetBlipCoords(blip);
@@ -42,12 +18,21 @@ setTick(() => {
         786603,
         10
       );
-      autoDriveActive = true;
-      return;
     }
-    autoDriveActive = false;
-  } else if (!autoDriveEnabled && autoDriveActive) {
-    Citizen.invokeNative("0xDBBC7A2432524127", vehicleId);
-    autoDriveActive = false;
-  }
-});
+  },
+  false
+);
+
+RegisterCommand(
+  "-autodrive",
+  () => {
+    const myPed = PlayerPedId();
+    const vehicleId = GetVehiclePedIsIn(myPed, false);
+    if (vehicleId) {
+      Citizen.invokeNative("0xDBBC7A2432524127", vehicleId);
+    }
+  },
+  false
+);
+
+RegisterKeyMapping("+autodrive", "Auto Drive", "keyboard", "b");
